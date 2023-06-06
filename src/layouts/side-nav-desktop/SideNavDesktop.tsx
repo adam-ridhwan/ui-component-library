63c9fbeed7058559d4cfc4a999ddbcf74f3b885c
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-06-06 11:35:19 -0400

    refactor: dry principle SideNavDesktop.tsx

--- src/layouts/side-nav-desktop/SideNavDesktop.tsx
+++ src/layouts/side-nav-desktop/SideNavDesktop.tsx
@@ -1,12 +1,31 @@
-import NavigationLink from '@/components/navigation-link/NavigationLink.tsx';
+import { FC } from 'react';
+import NavigationLink from '@/components/navigation-link/NavigationLink';
 import { useSideBarContext } from '@/hooks/useSideBarContext';
 import { COMPONENTS, COMPONENTS_ROUTES, DOC_ROUTE, DOCUMENTATION } from '@/utils/constants';
 import { convertToTitleCase } from '@/utils/convertToTitleCase';
 
 import styles from './SideNavDesktop-styles.module.css';
 
-const DesktopSideNavBar = () => {
+const DesktopSideNavBar: FC = () => {
   const { currentSection } = useSideBarContext();
+
+  const renderNavigationLinks = (items: string[], basePath: string, isDoc: boolean) => {
+    return items.map((item, index) => {
+      const path = isDoc && item === 'introduction' ? DOC_ROUTE : `${basePath}/${item}`;
+      const section = isDoc && item === 'introduction' ? 'docs' : item;
+
+      return (
+        <NavigationLink
+          key={index}
+          path={path}
+          section={section}
+          className={currentSection === section ? styles.navigation_link_active : styles.navigation_link_not_active}
+        >
+          <span>{convertToTitleCase(item)}</span>
+        </NavigationLink>
+      );
+    });
+  };
 
   return (
     <>
@@ -14,56 +33,17 @@ const DesktopSideNavBar = () => {
         <div className={styles.container}>
           <div className={styles.instructions_container}>
             <span className={styles.title}>Getting started</span>
-            {DOCUMENTATION.map((section, index) => {
-              if (section === 'introduction')
-                return (
-                  <NavigationLink
-                    key={index}
-                    path={`${DOC_ROUTE}`}
-                    section="docs"
-                    className={`${
-                      currentSection === 'docs' ? styles.navigation_link_active : styles.navigation_link_not_active
-                    }`}
-                  >
-                    <span>{convertToTitleCase(section)}</span>
-                  </NavigationLink>
-                );
-
-              return (
-                <NavigationLink
-                  key={index}
-                  path={`${DOC_ROUTE}/${section}`}
-                  section={section}
-                  className={`${
-                    currentSection === section ? styles.navigation_link_active : styles.navigation_link_not_active
-                  }`}
-                >
-                  <span>{convertToTitleCase(section)}</span>
-                </NavigationLink>
-              );
-            })}
+            {renderNavigationLinks(DOCUMENTATION, DOC_ROUTE, true)}
           </div>
 
           <div className={styles.components_container}>
             <span className={styles.title}>Components</span>
-            {COMPONENTS.map((component, index) => {
-              return (
-                <NavigationLink
-                  key={index}
-                  path={`${COMPONENTS_ROUTES}/${component}`}
-                  section={component}
-                  className={`${
-                    currentSection === component ? styles.navigation_link_active : styles.navigation_link_not_active
-                  }`}
-                >
-                  <span>{convertToTitleCase(component)}</span>
-                </NavigationLink>
-              );
-            })}
+            {renderNavigationLinks(COMPONENTS, COMPONENTS_ROUTES, false)}
           </div>
         </div>
       </aside>
     </>
   );
 };
+
 export default DesktopSideNavBar;
