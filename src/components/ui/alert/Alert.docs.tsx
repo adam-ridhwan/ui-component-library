6ec20b51baba4b324ec0d61e06c361b062a6408d
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-06-21 11:37:23 -0400

    style: fix

--- src/components/ui/alert/Alert.docs.tsx
+++ src/components/ui/alert/Alert.docs.tsx
@@ -1,21 +1,19 @@
-import ContentContainer from '@/components/containers/content-container/ContentContainer.tsx';
-import SectionContainer from '@/components/containers/section-container/SectionContainer.tsx';
-import Breadcrumbs from '@/components/breadcrumbs/Breadcrumbs.tsx';
-import Heading from '@/components/containers/typography/heading/Heading.tsx';
-import Subheading from '@/components/containers/typography/subheading/Subheading.tsx';
-import Divider from '@/layouts/divider/Divider.tsx';
-import TabSelector from '@/layouts/tab-selector';
-import ComponentContainer from '@/components/containers/component-container/ComponentContainer.tsx';
-import { Tab, useTab } from '@/hooks/useTab.ts';
-import PaginationContainer from '@/components/containers/pagination-container/PaginationContainer.tsx';
-import NavigationLink from '@/components/navigation-link/NavigationLink.tsx';
-import ChevronRightIcon from '@/assets/svg/ChevronRightIcon.tsx';
-import ChevronLeftIcon from '@/assets/svg/ChevronLeftIcon.tsx';
-import QuickNavContainer from '@/components/containers/quick-nav-container/QuickNavContainer.tsx';
-import QuickNav from '@/layouts/quick-nav/QuickNav.tsx';
-import { COMPONENTS } from '@/utils/constants.ts';
-import { convertToTitleCase } from '@/utils/convertToTitleCase.ts';
-import { getPaginationIndex } from '@/utils/getPaginationIndex.ts';
+import ContentContainer from '@/components/containers/content-container/ContentContainer';
+import SectionContainer from '@/components/containers/section-container/SectionContainer';
+import Breadcrumbs from '@/components/breadcrumbs/Breadcrumbs';
+import Heading from '@/components/containers/typography/heading/Heading';
+import Subheading from '@/components/containers/typography/subheading/Subheading';
+import Divider from '@/layouts/divider/Divider';
+import ComponentContainer from '@/components/containers/component-container/ComponentContainer';
+import PaginationContainer from '@/components/containers/pagination-container/PaginationContainer';
+import NavigationLink from '@/components/navigation-link/NavigationLink';
+import ChevronRightIcon from '@/assets/svg/ChevronRightIcon';
+import ChevronLeftIcon from '@/assets/svg/ChevronLeftIcon';
+import QuickNavContainer from '@/components/containers/quick-nav-container/QuickNavContainer';
+import QuickNav from '@/layouts/quick-nav/QuickNav';
+import { COMPONENTS } from '@/utils/constants';
+import { convertToTitleCase } from '@/utils/convertToTitleCase';
+import { getPaginationIndex } from '@/utils/getPaginationIndex';
 
 const AlertDocs = () => {
   const [PREVIOUS_INDEX, NEXT_INDEX] = getPaginationIndex('alert');
